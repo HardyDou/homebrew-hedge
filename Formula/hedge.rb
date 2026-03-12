a1a2c1dc33d9aa53bf5c573c01b77c648b9cd78b
hi31791423@aliyun.com <doouxiaoyu@163.com>
2026-03-12 17:48:45 +0800

    Fix: update URLs to v1.9.1

--- Formula/hedge.rb
+++ Formula/hedge.rb
@@ -6,17 +6,17 @@ class Hedge < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/HardyDou/hedge/releases/download/v1.9.0/hedge-cli-macos-arm64.tar.gz"
-      sha256 "af727b03f1b1863385edba59a6ab1ddc076850eb444e41f4b8c257d735dee202" # Will be filled after first release
+      url "https://github.com/HardyDou/hedge/releases/download/v1.9.1/hedge-cli-macos-arm64.tar.gz"
+      sha256 "af727b03f1b1863385edba59a6ab1ddc076850eb444e41f4b8c257d735dee202"
     else
-      url "https://github.com/HardyDou/hedge/releases/download/v1.9.0/hedge-cli-macos-x64.tar.gz"
+      url "https://github.com/HardyDou/hedge/releases/download/v1.9.1/hedge-cli-macos-x64.tar.gz"
       sha256 "" # Will be filled after first release
     end
   end
 
   on_linux do
-    url "https://github.com/HardyDou/hedge/releases/download/v1.9.0/hedge-cli-linux-x64.tar.gz"
-    sha256 "7925b8e7fdbe62c0671962b460ce099bd0da4f44a62e21b5b5d8b1929fd25cc4" # Will be filled after first release
+    url "https://github.com/HardyDou/hedge/releases/download/v1.9.1/hedge-cli-linux-x64.tar.gz"
+    sha256 "7925b8e7fdbe62c0671962b460ce099bd0da4f44a62e21b5b5d8b1929fd25cc4"
   end
 
   def install
